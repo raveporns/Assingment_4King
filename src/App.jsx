2ddf5d704a9ac3337c679c9094ff160d4829f9c1
raveporns <ravepornsppc@gmail.com>
2024-03-15 21:14:40 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+
 
 function App() {
   const [quantity, setQuantity] = useState(0)
